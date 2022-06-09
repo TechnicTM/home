b77e63c2e9b140c83401f366e542927262a7655c
TechnicTM <105014747+TechnicTM@users.noreply.github.com>
2022-06-09 16:45:46 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-//menu nav
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
-const menuBlack = document.querySelector('.blacknav');
+
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-    menuBlack.classList.toggle('active');
 })
 
-//discord tab
-function discorddisplay(){
-    const dis = document.querySelector('.discordtab');
-    dis.classList.toggle('activate');
+window.addEventListener("scroll", function(){
+    const navContainer = document.querySelector('.nav-container');
+    const navbar = document.querySelector('.navbar');
+    const navMenu = document.querySelector('.nav-menu');
+    const langb = document.querySelector('.languageb');
+    navContainer.classList.toggle('sticky', window.scrollY > 50);
+    navbar.classList.toggle('sticky', window.scrollY > 50);
+    navMenu.classList.toggle('sticky', window.scrollY > 50);
+    langb.classList.toggle('sticky', window.scrollY > 50);
+})
+
+function language(){
+    const lang = document.querySelector('.lang-menu');
+    //const langbtn = document.querySelector('.languagebtn');
+    lang.classList.toggle('activate');
+    //langbtn.classList.toggle('activate');
 }
 
-//video toggle
-function toggleVideo(){
-    const video = document.querySelector('.video');
-    const vid = document.querySelector('.videobox');
-    video.classList.toggle('activity')
-    vid.currentTime = 0;
-    vid.pause();
-}
-
-//Game Snackbar
-function Game() {
-  let gs = document.getElementById("snackbar-cs");
-  gs.className = "show";
-  setTimeout(function(){ gs.className = gs.className.replace("show", ""); }, 3000);
-}
+const textarea = document.querySelector('textarea');
+textarea.addEventListener("keyup", e =>{
+    let scHeight = e.target.scrollHeight;
+    textarea.style.height = `${scHeight}px`;
+});
